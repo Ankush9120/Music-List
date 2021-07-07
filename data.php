3014c7b38b99bbae53848fe97e353ab944c64e91
Ankush9120 <70263939+Ankush9120@users.noreply.github.com>
2021-07-07 19:38:17 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,30 @@
+<?php
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+$data="$name";
+
+
+$file="mydata.txt";
+
+
+file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+print "<h1 align=center>Thank you for submitting your Details</h1>";
+
+
+?>
+<html>
+<head>
+<title>ThankYou Page</title>    
+</head>
+<body>
+<h1><a href="index.html">GO BACK</a> </h1>    
+    
+</body>
+
+
+
+
+</html>
